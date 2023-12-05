f00bb28bb560c5f7eafb9b4ca257c6457cda49da
grez-lucas <lucasgrez3010@gmail.com>
2023-12-05 15:23:35 -0300

    refactor: app theme is now more grounded-fantasy

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,16 +1,22 @@
-
-
-import 'package:flutter/material.dart' ;
+import 'package:flutter/material.dart';
 
 class AppTheme {
-
-
   static const String fontName = 'Poppins';
 
-  static const Color primaryColor = Color.fromARGB(255, 219, 218, 168);
+  static const Color primaryColor =
+      Color.fromARGB(255, 139, 69, 19) // Earthy Brown
+// Eldritch Blue
+      // Dark Enchantment
+      // Mystical Indigo
+      ;
+
+  static const Color backgroundColor =
+      Color.fromARGB(255, 128, 128, 128); // Mystical Gray
 
   static final ThemeData lightTheme = ThemeData(
     primaryColor: primaryColor,
+    colorScheme:
+        const ColorScheme.light(background: Color.fromARGB(255, 206, 170, 168)),
     brightness: Brightness.light,
     fontFamily: fontName,
     appBarTheme: const AppBarTheme(
@@ -18,5 +24,11 @@ class AppTheme {
       elevation: 0,
       foregroundColor: Colors.white,
     ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: primaryColor,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.white.withOpacity(.60),
+    ),
+    scaffoldBackgroundColor: backgroundColor,
   );
 }
