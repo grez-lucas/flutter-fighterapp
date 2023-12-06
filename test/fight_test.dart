9a627a5f02c9ea99e62af7ff74c6c481479dc85d
grez-lucas <lucasgrez3010@gmail.com>
2023-12-06 12:23:59 -0300

    tests: random fighters on basic fight test

--- test/fight_test.dart
+++ test/fight_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:category_app2/models/models.dart';
@@ -19,15 +21,24 @@ void main() {
 
   test('Test Fight startFight() method', () {
     final fighters = FighterModel.getFighters();
-    final fight = Fight(fighter1: fighters[0], fighter2: fighters[1]);
+
+    FighterModel fighter1;
+    FighterModel fighter2;
+
+    do {
+      fighter1 = fighters[Random().nextInt(fighters.length)];
+      fighter2 = fighters[Random().nextInt(fighters.length)];
+    } while (fighters[0].name == fighters[1].name);
+
+    final fight = Fight(fighter1: fighter1, fighter2: fighter2);
 
     // print(fighters[0].stats);
 
     fight.startFight();
 
-    for (var log in fight.log) {
+    fight.log.asMap().forEach((index, log) {
       print('Turn ${fight.turns.length}: $log\n');
-    }
+    });
 
     // expect(fight.turns.length, 3);
     // expect(fight.log.length, 4);
